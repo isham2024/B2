9fb4046577b91d76a6b477c6f58cb5b11b07f62e
isham2024 <132916476+isham2024@users.noreply.github.com>
2023-05-18 23:32:03 +0530

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,21 +1,21 @@
-declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            // Server-side environment variables should be named using uppercase
-            // letters only, while camelCase naming convention should be used
-            // for variables shared by both the client-side and server-side.
-            appName: string;
-            appLogo: string;
-            appThemeColor: string;
-            appSummary: string;
-            exampleInput: string;
+# Client side
 
-            OPENAI_API_KEY: string;
-            OPENAI_API_BASE_URL: string;
-            SYSTEM_MESSAGE: string;
-            MESSAGE_TEMPLATE: string;
-        }
-    }
-}
+# The app logo url.
+APP_LOGO="https://www.flaticon.com/free-icon/accenture_6128893"
+# The name of the app.
+APP_NAME="ARCH"
+# Summary your app's behavoir and show the users how to use it.
+APP_SUMMARY="Stay up-to-date on your firm-specific regulations"
+# Example input that shows user how to use the app.
+EXAMPLE_INPUT="What is Basel III?"
 
-export {}
+# Server side
+
+# Required, the API key got from OpenAI (https://platform.openai.com/account/api-keys)
+OPENAI_API_KEY="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"
+# Optional, the agent server of OpenAI API. Use this when the offical OpenAI API server is unreachable.
+OPENAI_API_BASE_URL=""
+# Optional, the system message helps set the behavior of the assistant. (Learn more from https://platform.openai.com/docs/guides/chat/introduction)
+SYSTEM_MESSAGE="Learn about regulations specific to your firm"
+# Optional, the message template to wrap the user inputs, the `{{input}}` string in the template will be replaced by user inputs.
+MESSAGE_TEMPLATE="Enter your regulatory requirement"
